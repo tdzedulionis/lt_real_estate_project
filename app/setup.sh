e7fc8c2e7d0d0c8d80bf581e72acc6868c38c47c
tdzedulionis <tomas.dzedulionis@gmail.com>
2025-05-06 19:14:35 +0300

    upd streamlit

---
+++ app/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Add Microsoft repository
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+# Install ODBC Driver
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
