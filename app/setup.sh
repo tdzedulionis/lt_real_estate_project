c0b2015cb8525ec7c0c6de56caf743f141ca040c
tdzedulionis <tomas.dzedulionis@gmail.com>
2025-05-28 12:20:21 +0300

    fixing sql

--- app/setup.sh
+++ app/setup.sh
@@ -4,7 +4,7 @@
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
-# Install ODBC Driver 17
+# Install ODBC Driver 18
 apt-get update
 ACCEPT_EULA=Y apt-get install -y msodbcsql18
 
